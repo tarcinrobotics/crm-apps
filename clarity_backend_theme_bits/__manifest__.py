8e02905bcab22305f56c0b23a447e74d63fceb39
tarcinrobotics <devops@tarcincacademy.in>
2024-03-13 01:31:56 +0530

    added files for themes

---
+++ clarity_backend_theme_bits/__manifest__.py
@@ -0,0 +1,119 @@
+{
+    "name": "Clarity Backend Theme for community",
+    "version": "17.0.1.0.1",
+    'author': "Terabits Technolab",
+    'summary': """   
+        Clarity backend theme  
+
+        Simplify Access Management | Manage - Hide Menu, Submenu, Fields, Action, Reports, Views, | Restrict/Read-Only User, Apps,  Fields, Export, Archive, Actions, Views, Reports, Delete items | Manage Access rights from one place | Hide Tabs and buttons | Multi Company supported.
+        Delight POS theme | Advance POS Theme | point of sale theme | Theme POS | Odoo POS theme | Point of sale split bill | Point of sale split order | POS split bill | POS split invoices |
+        Unlock the true potential of your data with AnalytiX Dashboard, a cutting-edge Odoo dashboard module designed to revolutionize the way you visualize and analyze information. Elevate your decision-making process with intuitive, interactive, and insightful dashboards that empower you to confidently navigate your business landscape.|  AnalytiX Dashboard | Dashboard manager | All in One Dashboard | Simplified dashboard manager |  Sales Dashboard manager | CRM dashboard manager | 
+        HR dashboard manager | Inventory dashboard manager | Product dashboard manager | webiste dashboard manager | support dashboard | events dashboard manager.
+        AnalytiX Dashboard | Dashboard manager | All in One Dashboard | Simplified dashboard manager |  purchase dashboard manager | CRM dashboard manager | 
+        HR dashboard manager | Inventory dashboard manager | Product dashboard manager | webiste dashboard manager | support dashboard | events dashboard manager 
+        You can create a Sales Dashboard, Inventory Dashboard, Finance Dashboard, or Others dynamically with this module. |
+        KPI dashboards. Dashboard designer. KPI charts. Odoo dashboards. Analytic dashboards. Create dashboards. Customize dashboards. Chart Graphs. Key performance indicators. Dynamic KPIs. Smart goals.|
+        Bar chart| Line chart | Statistics Chart | Trend Chart| Dashboard Ninja Advance | Dashboard Ninja with ai
+        Get smart visual data with interactive and engaging dashboards for your Odoo ERP.|Odoo Dashboard, CRM Dashboard, Inventory Dashboard, Sales Dashboard, Account Dashboard, Invoice Dashboard, Revamp Dashboard,
+        Best Dashboard, Odoo Best Dashboard, Odoo Apps Dashboard, Best Ninja Dashboard, Analytic Dashboard, Pre-Configured Dashboard, Create Dashboard, Beautiful Dashboard, Customized Robust Dashboard, Predefined Dashboard, 
+        Multiple Dashboards, Advance Dashboard, Beautiful Powerful Dashboards, Chart Graphs Table View, All In One Dynamic Dashboard, Accounting Stock Dashboard, Pie Chart Dashboard, Modern Dashboard, Dashboard Studio, 
+        Dashboard Builder, Dashboard Designer, Odoo Studio,Open hrms | crm | sales | inventrory | point of sales | modern odoo dashboard | data analysis | dynamic odoo dashboard | Create Own Analytic Odoo dashboards |
+        account dashboard | employee dashboard | employee leave analysis | activity dashbord | ceo dashboard | manager dashboard | HR dashboard | marketing
+
+        Odoo Backend Theme, Odoo Community Backend Theme, Web backend Theme, Web Responsive Odoo Theme, New theme design, New design, Multi Level Menu,
+        Web Responsive Odoo Backend Theme, Odoo Theme, Odoo Modern Theme, Odoo Modern Backend Theme Odoo, Advance Theme Backend Advanced, Sidebar Light,
+        All in one, New advanced Odoo Menu, Sidebar apps, New design, Left sidebar menu, Web menu, Odoo backend menu, Web Responsive menu, Sidebar White,
+        Advance Menu Odoo App Menu Apps, Advanced Apps Menu, Elegant Menu, Menuitem, Web App Menu Backend, Menu Odoo Backend, Collapse Menu, Light Sidebar,
+        Expand Menu, Collapsed Menu, Expanded Menu, New Style Menus, Advanced Sidebar Menu, Advance Sidebar Menu, Responsive Menu Sidebar, Sidebar Theme,
+        Responsive Sidebar, Hide menu, Show Menu, Hide Sidebar, Show Sidebar, Toggle Menu, Toggle Sidebar, All in one Dynamic Menu Access, Menu Theme,
+        Visibility Menu Visibility, Quick Backend Menu, Dropdown Menu, Parent Menus, Shortcut Menus Shortcut, Menu Icons, Collapsible menu Odoo,
+        Customize Menu Customize Sidebar App, Customization Menu Customization App Sidebar Customization Sidebar Apps, Group Left Menu in Odoo,
+        Global Search Menu Search, Global Menu Access Global Apps Menu Global, Group Top Menu in Odoo, Odoo Foldable Menu Applications, Navbar,
+        App web Menu, Quick Menu Access Menu, Menu Dynamic Sidebar, Any menu, Easy Access for menuitems, User Menu Users, All in one Sidebar,
+        Advanced Menu, Advanced Odoo Menu Backend Odoo Web Theme Web Odoo, Elegant Theme Simple Theme, Advance List View Manager, Menu Style,
+        Advanced List View Advanced Pivot View Theme Table View Theme Form View Theme Advanced Forms, Beautiful Theme Design, New Style Theme
+    
+        The ultimate Odoo Backend theme with the most advanced key features of all time. Get your own personalized view while working on the Backend system with a wide range of choices. Spiffy theme has 3 in 1 Theme Style, Progressive Web App, Fully Responsive for all apps, Configurable Apps Icon, App Drawer with global search, RTL & Multi-Language Support, and many other key features.  
+
+        Ditch the boring theme while working; opt for the Odoo Arc Backend Theme and increase your business performance.
+        Switch from Light and Dark mode as per your working necessities. Arc Backend Theme, Backend Theme, Responsive Theme, 
+        Fully Functional Theme, Flexible Backend Theme, Fast Backend Theme, Modern Multipurpose Theme, Lightweight Backend Theme, 
+        Animated Backend Theme, Advance Material Backend Theme, Customizable Backend Theme, Multi Tab Backend Theme Odoo, 
+        Attractive Theme for Backend, Elegant Backend Theme, Community Backend Theme, Odoo Community Backend Theme, 
+        Fully Functional Backend Theme, Responsive Web Client, Mobile Theme, Backend UI, Mobile Interface,
+        Mobile Responsive for Odoo Community, Dual Color Backend Theme, Flexible Enterprise Theme, Enterprise Backend Theme
+    """,
+    'description': """ 
+        Clarity backend theme  
+
+        Simplify Access Management | Manage - Hide Menu, Submenu, Fields, Action, Reports, Views, | Restrict/Read-Only User, Apps,  Fields, Export, Archive, Actions, Views, Reports, Delete items | Manage Access rights from one place | Hide Tabs and buttons | Multi Company supported.
+        Delight POS theme | Advance POS Theme | point of sale theme | Theme POS | Odoo POS theme | Point of sale split bill | Point of sale split order | POS split bill | POS split invoices |
+        Unlock the true potential of your data with AnalytiX Dashboard, a cutting-edge Odoo dashboard module designed to revolutionize the way you visualize and analyze information. Elevate your decision-making process with intuitive, interactive, and insightful dashboards that empower you to confidently navigate your business landscape.|  AnalytiX Dashboard | Dashboard manager | All in One Dashboard | Simplified dashboard manager |  Sales Dashboard manager | CRM dashboard manager | 
+        HR dashboard manager | Inventory dashboard manager | Product dashboard manager | webiste dashboard manager | support dashboard | events dashboard manager.
+        AnalytiX Dashboard | Dashboard manager | All in One Dashboard | Simplified dashboard manager |  purchase dashboard manager | CRM dashboard manager | 
+        HR dashboard manager | Inventory dashboard manager | Product dashboard manager | webiste dashboard manager | support dashboard | events dashboard manager 
+        You can create a Sales Dashboard, Inventory Dashboard, Finance Dashboard, or Others dynamically with this module. |
+        KPI dashboards. Dashboard designer. KPI charts. Odoo dashboards. Analytic dashboards. Create dashboards. Customize dashboards. Chart Graphs. Key performance indicators. Dynamic KPIs. Smart goals.|
+        Bar chart| Line chart | Statistics Chart | Trend Chart| Dashboard Ninja Advance | Dashboard Ninja with ai
+        Get smart visual data with interactive and engaging dashboards for your Odoo ERP.|Odoo Dashboard, CRM Dashboard, Inventory Dashboard, Sales Dashboard, Account Dashboard, Invoice Dashboard, Revamp Dashboard,
+        Best Dashboard, Odoo Best Dashboard, Odoo Apps Dashboard, Best Ninja Dashboard, Analytic Dashboard, Pre-Configured Dashboard, Create Dashboard, Beautiful Dashboard, Customized Robust Dashboard, Predefined Dashboard, 
+        Multiple Dashboards, Advance Dashboard, Beautiful Powerful Dashboards, Chart Graphs Table View, All In One Dynamic Dashboard, Accounting Stock Dashboard, Pie Chart Dashboard, Modern Dashboard, Dashboard Studio, 
+        Dashboard Builder, Dashboard Designer, Odoo Studio,Open hrms | crm | sales | inventrory | point of sales | modern odoo dashboard | data analysis | dynamic odoo dashboard | Create Own Analytic Odoo dashboards |
+        account dashboard | employee dashboard | employee leave analysis | activity dashbord | ceo dashboard | manager dashboard | HR dashboard | marketing
+
+        Odoo Backend Theme, Odoo Community Backend Theme, Web backend Theme, Web Responsive Odoo Theme, New theme design, New design, Multi Level Menu,
+        Web Responsive Odoo Backend Theme, Odoo Theme, Odoo Modern Theme, Odoo Modern Backend Theme Odoo, Advance Theme Backend Advanced, Sidebar Light,
+        All in one, New advanced Odoo Menu, Sidebar apps, New design, Left sidebar menu, Web menu, Odoo backend menu, Web Responsive menu, Sidebar White,
+        Advance Menu Odoo App Menu Apps, Advanced Apps Menu, Elegant Menu, Menuitem, Web App Menu Backend, Menu Odoo Backend, Collapse Menu, Light Sidebar,
+        Expand Menu, Collapsed Menu, Expanded Menu, New Style Menus, Advanced Sidebar Menu, Advance Sidebar Menu, Responsive Menu Sidebar, Sidebar Theme,
+        Responsive Sidebar, Hide menu, Show Menu, Hide Sidebar, Show Sidebar, Toggle Menu, Toggle Sidebar, All in one Dynamic Menu Access, Menu Theme,
+        Visibility Menu Visibility, Quick Backend Menu, Dropdown Menu, Parent Menus, Shortcut Menus Shortcut, Menu Icons, Collapsible menu Odoo,
+        Customize Menu Customize Sidebar App, Customization Menu Customization App Sidebar Customization Sidebar Apps, Group Left Menu in Odoo,
+        Global Search Menu Search, Global Menu Access Global Apps Menu Global, Group Top Menu in Odoo, Odoo Foldable Menu Applications, Navbar,
+        App web Menu, Quick Menu Access Menu, Menu Dynamic Sidebar, Any menu, Easy Access for menuitems, User Menu Users, All in one Sidebar,
+        Advanced Menu, Advanced Odoo Menu Backend Odoo Web Theme Web Odoo, Elegant Theme Simple Theme, Advance List View Manager, Menu Style,
+        Advanced List View Advanced Pivot View Theme Table View Theme Form View Theme Advanced Forms, Beautiful Theme Design, New Style Theme
+    
+        The ultimate Odoo Backend theme with the most advanced key features of all time. Get your own personalized view while working on the Backend system with a wide range of choices. Spiffy theme has 3 in 1 Theme Style, Progressive Web App, Fully Responsive for all apps, Configurable Apps Icon, App Drawer with global search, RTL & Multi-Language Support, and many other key features.  
+
+        Ditch the boring theme while working; opt for the Odoo Arc Backend Theme and increase your business performance.
+        Switch from Light and Dark mode as per your working necessities. Arc Backend Theme, Backend Theme, Responsive Theme, 
+        Fully Functional Theme, Flexible Backend Theme, Fast Backend Theme, Modern Multipurpose Theme, Lightweight Backend Theme, 
+        Animated Backend Theme, Advance Material Backend Theme, Customizable Backend Theme, Multi Tab Backend Theme Odoo, 
+        Attractive Theme for Backend, Elegant Backend Theme, Community Backend Theme, Odoo Community Backend Theme, 
+        Fully Functional Backend Theme, Responsive Web Client, Mobile Theme, Backend UI, Mobile Interface,
+        Mobile Responsive for Odoo Community, Dual Color Backend Theme, Flexible Enterprise Theme, Enterprise Backend Theme
+    """,
+    "sequence": 7,
+    "license": "OPL-1",
+    "category": "Themes/Backend",
+    "website": "https://www.terabits.xyz",
+    "depends": ["web"],
+    "data": [ 
+        'views/res_config_setting.xml',
+        'views/res_users.xml',
+        'views/webclient_templates.xml'
+    ],
+    "assets": {
+        "web.assets_frontend": [
+            'clarity_backend_theme_bits/static/src/scss/login.scss'
+        ],
+        "web.assets_backend": [   
+            'clarity_backend_theme_bits/static/src/xml/WebClient.xml',
+            'clarity_backend_theme_bits/static/src/xml/navbar/sidebar.xml', 
+            'clarity_backend_theme_bits/static/src/xml/systray_items/user_menu.xml',
+            'clarity_backend_theme_bits/static/src/js/SidebarBottom.js',  
+            'clarity_backend_theme_bits/static/src/js/WebClient.js', 
+            'clarity_backend_theme_bits/static/src/scss/layout.scss',
+            'clarity_backend_theme_bits/static/src/scss/navbar.scss', 
+            'clarity_backend_theme_bits/static/src/js/navbar.js',  
+        ],
+    }, 
+    'installable': True,
+    'application': True,
+    'auto_install': False,  
+    'images': [
+        'static/description/logo.gif',
+        'static/description/theme_screenshot.gif',
+    ],
+}
